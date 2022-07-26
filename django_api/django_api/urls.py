2d38ef113431ab2357c45e1bb77e4804c1f9f7ab
Ivan <ivanrezv@icloud.com>
2022-07-26 17:36:50 +0300

    views api requests with postman

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -5,6 +5,6 @@ from test_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('simple/', simple),
-    path('json/', json),
+    path('simple/', simple.as_view()),
+    # path('json/', json),
 ]
--- django_api/test_app/views.py
+++ django_api/test_app/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from django.http import JsonResponse, response
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from .models import TestModel
+from django.forms.models import model_to_dict
 
 
-def simple(request):
-    a = 30 + 50
-    return response.HttpResponse(f"<h1>{a}<h1>")
+class simple(APIView):
 
+    def post(self, request):
+        new_test_content = TestModel.objects.create(
+            name=request.data["name"],
+            description=request.data["description"],
+            phone_number=request.data["phone_number"],
+            is_alive=request.data["is_alive"],
+            amount=request.data["amount"]
+        )
+        return JsonResponse({"data": model_to_dict(new_test_content)})
 
-@csrf_exempt
-def json(request):
-    method = request.method.lower()
-    if method == "get":
-        return JsonResponse({"data": [1, 3, 4, 6, 5 , 6]})
-    elif method == "post":
-        return JsonResponse({"data": "added data suc"})
-    elif method == "put":
-        return JsonResponse({"data": "Updated data suc"})
-    return JsonResponse({"error": "Method " + request.method + " is not allowed"})
+    def get(self, request):
+        content = TestModel.objects.all().values()
+        return JsonResponse({"data": list(content)})
+
+# @csrf_exempt
+# def json(request):
+#     method = request.method.lower()
+#     if method == "get":
+#         return JsonResponse({"data": [1, 3, 4, 6, 5 , 6]})
+#     elif method == "post":
+#         return JsonResponse({"data": "added data suc"})
+#     elif method == "put":
+#         return JsonResponse({"data": "Updated data suc"})
+#     return JsonResponse({"error": "Method " + request.method + " is not allowed"})
