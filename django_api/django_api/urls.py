7727d607c1fadb342c9b5713a1c33847eebf083f
Ivan <ivanrezv@icloud.com>
2022-07-26 17:11:19 +0300

    views fst les with json response

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from test_app.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('simple/', simple),
+    path('json/', json),
 ]
--- django_api/test_app/views.py
+++ django_api/test_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse, response
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def simple(request):
+    a = 30 + 50
+    return response.HttpResponse(f"<h1>{a}<h1>")
+
+
+@csrf_exempt
+def json(request):
+    method = request.method.lower()
+    if method == "get":
+        return JsonResponse({"data": [1, 3, 4, 6, 5 , 6]})
+    elif method == "post":
+        return JsonResponse({"data": "added data suc"})
+    elif method == "put":
+        return JsonResponse({"data": "Updated data suc"})
+    return JsonResponse({"error": "Method " + request.method + " is not allowed"})
